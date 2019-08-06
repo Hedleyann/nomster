bfc29c55dcc5967a1aff04def4f86eec4c135e24
hedleyann <hedleyann@msn.com>
2019-08-06 03:01:22 +0000

    Updating the Place with New Data

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,12 +14,18 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
-  def 
+  def show
     @place = Place.find(params[:id])
   end
 
   def edit
     @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
   end
 
   private
