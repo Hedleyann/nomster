9ea3f8ad4f9c371cd7150e4a25aa8529f63fdccb
hedleyann <hedleyann@msn.com>
2019-09-03 21:20:49 +0000

    Adding comments

--- db/migrate/20190811231353_create_comments.rb
+++ db/migrate/20190811231353_create_comments.rb
@@ -1,11 +1,10 @@
-class CreateComments < ActiveRecord::Migration[5.2]
+class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.text :message
       t.string :rating
       t.integer :user_id
       t.integer :place_id
-      t.timestamps
       t.timestamps
     end
 
